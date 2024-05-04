9ad3f3d156b74f89c742ca5f71682f3ebde1bb83
aldalee <hadoop_2020@163.com>
2024-05-04 22:04:54 +0800

    respond to pages by template

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"text/template"
 )
 
 type IndexData struct {
@@ -20,10 +22,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(marshal)
 }
 
+func indexHtml(w http.ResponseWriter, r *http.Request) {
+	var indexData IndexData
+	indexData.Title = "Golang Minimalist Blog"
+	indexData.Desc = "Now it's very easy!"
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	file, _ := t.ParseFiles(path + "/template/index.html")
+	_ = file.Execute(w, indexData)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
 	log.Fatal(server.ListenAndServe())
 }
