801379ef22d0fcf3c2d00e423738980346bc6b1d
aldalee <hadoop_2020@163.com>
2024-05-04 20:22:39 +0800

    custom handler

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
+
+type IndexData struct {
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var indexData IndexData
+	indexData.Title = "Golang Minimalist Blog"
+	indexData.Desc = "Now it's very easy!"
+	marshal, _ := json.Marshal(indexData)
+	_, _ = w.Write(marshal)
+}
 
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Hello World!"))
-	})
+	http.HandleFunc("/", index)
 	log.Fatal(server.ListenAndServe())
 }
